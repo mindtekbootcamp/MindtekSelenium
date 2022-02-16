98d09127cfe5d787331ae81a4bde32e82557e096
parahatoraz <oraz.parhat@gmail.com>
2022-02-16 16:17:48 -0600

    test

--- src/locators/Test.java
+++ src/locators/Test.java
@@ -2,9 +2,12 @@ package locators;
 
 public class Test {
     public static void main(String[] args) {
+
         System.out.println("test");
-
-
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
 
     }
 }
