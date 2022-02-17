dd99aefed352ba27e169d958835183c74c8ecef2
parahatoraz <oraz.parhat@gmail.com>
2022-02-16 19:18:47 -0600

    messsage

--- src/locators/CSSTest.java
+++ src/locators/CSSTest.java
@@ -5,33 +5,42 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 public class CSSTest {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver","/Users/parahatoraz/repo/MindtekSelenium/chromedriver");
 
         WebDriver driver=new ChromeDriver();
 
+
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete11/WebOrders/login.aspx");
         driver.manage().window().maximize();
 
-         WebElement loginButton= driver.findElement(By.id("ctl00_MainContent_login_button"));
-         loginButton.click();
+        WebElement loginButton= driver.findElement(By.id("ctl00_MainContent_login_button"));
+        //loginButton.click();
 
         String errorMessage= driver.findElement(By.cssSelector("form span")).getText();
         System.out.println(errorMessage);
 
         WebElement userName= driver.findElement(By.cssSelector("input[id*='user']"));
-        userName.click();
+        userName.sendKeys("Tester");
+
+        WebElement password=driver.findElement(By.cssSelector("input[id*='pass']"));
+        password.sendKeys("test");
 
 
 
+        loginButton.click();
+
+        driver.quit();
 
 
-
-
+        System.out.println("PASSED");
 
 
     }
---
+++ src/locators/TestXpath.java
@@ -0,0 +1,24 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestXpath {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","/Users/parahatoraz/repo/MindtekSelenium/chromedriver");
+
+        WebDriver driver=new ChromeDriver();
+
+
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete11/WebOrders/login.aspx");
+        driver.manage().window().maximize();
+
+       WebElement userName= driver.findElement(By.xpath("//input[@id='ctl00_MainContent_username']"));
+
+
+    }
+}
