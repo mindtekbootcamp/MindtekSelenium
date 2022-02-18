342197cfe96bd28a14da28e2c04797def18e0611
parahatoraz <oraz.parhat@gmail.com>
2022-02-17 18:54:55 -0600

    Automation

--- src/testSelenium2/TextTest.java
+++ src/testSelenium2/TextTest.java
@@ -1,6 +1,8 @@
 package testSelenium2;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TextTest {
@@ -14,6 +16,19 @@ public class TextTest {
         driver.manage().window().maximize();
 
         driver.get("https://demo.seleniumeasy.com/basic-radiobutton-demo.html");
+
+
+        // print Text ==>  Click on button to get the selected value
+
+       WebElement text= driver.findElement(By.xpath("//p[text()='Click on button to get the selected value']"));
+       String  message=text.getText();
+       System.out.println(message);
+
+        //print Male text
+        System.out.println(driver.findElement(By.xpath("//input[@name='optradio'] [@value='Male']")).getAttribute("value"));
+
+
+
 
 
     }
