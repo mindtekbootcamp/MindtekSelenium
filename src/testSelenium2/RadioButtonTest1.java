9c6e1e9981936ba318dd365f45fcce2f98f66c31
parahatoraz <oraz.parhat@gmail.com>
2022-02-17 18:31:39 -0600

    Automation

---
+++ src/testSelenium2/RadioButtonTest1.java
@@ -0,0 +1,38 @@
+package testSelenium2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RadioButtonTest1 {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","/Users/parahatoraz/repo/MindtekSelenium/chromedriver");
+
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://demo.seleniumeasy.com/basic-radiobutton-demo.html");
+
+        WebElement maleRadioButton = driver.findElement(By.xpath("//input[@name='optradio'] [@value='Male']"));
+        maleRadioButton.click();
+
+        WebElement button= driver.findElement(By.cssSelector("button#buttoncheck"));
+        button.click();
+
+        WebElement text=driver.findElement(By.cssSelector("p.radiobutton"));
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
