7cded03dbcb61ca660b6361441de3e3bd0118d1a
parahatoraz <oraz.parhat@gmail.com>
2022-02-17 20:19:19 -0600

    Automation

---
+++ src/testSelenium2/CheckBoxTest2.java
@@ -0,0 +1,29 @@
+package testSelenium2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class CheckBoxTest2 {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","/Users/parahatoraz/repo/MindtekSelenium/chromedriver");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demo.seleniumeasy.com/basic-checkbox-demo.html");
+
+
+         List<WebElement> allOptions= driver.findElements(By.xpath("//input[@class='cb1-element']"));
+         System.out.println(allOptions.size());
+
+        for (int i = 0; i <allOptions.size() ; i++) {
+            allOptions.get(i).click();
+        }
+
+
+
+    }
+}
