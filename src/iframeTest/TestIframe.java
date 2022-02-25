51a863d588ebeb8f1b241330cb9f9cd040d4eb20
parahatoraz <oraz.parhat@gmail.com>
2022-02-24 19:36:18 -0600

    iframe

--- src/iframeTest/TestIframe.java
+++ src/iframeTest/TestIframe.java
@@ -1,8 +1,10 @@
 package iframeTest;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class TestIframe {
 
@@ -18,6 +20,39 @@ public class TestIframe {
 
         driver.get("https://the-internet.herokuapp.com/frames");
     }
+
+
+   // @Test
+    public void test1(){
+
+        driver.findElement(By.linkText("iFrame")).click();
+        driver.switchTo().frame(driver.findElement(By.id("mce_0_ifr")));
+        String text = driver.findElement(By.xpath("//p[text()='Your content goes here.']")).getText();
+
+        System.out.println(text);
+
+        driver.switchTo().defaultContent();
+        //driver.switchTo().parentFrame();
+        driver.findElement(By.linkText("Elemental Selenium")).click();
+
+    }
+
+    @Test
+    public void getSizeOfFrame(){
+
+        driver.findElement(By.linkText("iFrame")).click();
+
+       int sizeofFrames = driver.findElements(By.tagName("iFrame")).size();
+
+        System.out.println(sizeofFrames);
+
+
+
+
+    }
+
+
+
 
 
 
