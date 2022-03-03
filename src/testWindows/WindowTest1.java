5ed61c44a8e922a3516e1436430a6dd12a3ccf1e
parahatoraz <oraz.parhat@gmail.com>
2022-03-02 19:54:57 -0600

    window handles

--- src/testWindows/WindowTest1.java
+++ src/testWindows/WindowTest1.java
@@ -1,11 +1,16 @@
 package testWindows;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class WindowTest1 {
@@ -23,14 +28,72 @@ public class WindowTest1 {
     }
 
     @Test
-    public void test1(){
-
-       String windowId = driver.getWindowHandle();
-
-       System.out.println(windowId);
+    public void getWindowId(){
+        String windowId = driver.getWindowHandle();
+         System.out.println(windowId);
         //CDwindow-E102F1B6A0904FABE62E041174889AB3
         //CDwindow-C96ADA7D4A01115D88FDD84C330DB245
     }
+
+    @Test
+    public void getWindowIds(){
+
+        //1
+        WebElement link = driver.findElement(By.xpath("//a[@href='/windows/new']"));
+        link.click();
+
+       Set<String> windowIds = driver.getWindowHandles();
+
+       Iterator<String> it = windowIds.iterator();
+       String parentId = it.next();
+       String childId = it.next();
+
+       System.out.println("Parent window ID "+ parentId);
+       System.out.println("Child window ID "+ childId);
+
+       driver.switchTo().window(parentId);
+       System.out.println( "This is parent window title "+driver.getTitle());
+
+       driver.switchTo().window(childId);
+        System.out.println(driver.getTitle());
+
+    }
+
+
+    @Test
+    public void verifyChildWindowLogo(){
+        // OPEN SECOND WINDOW
+        // VERIFY "NEW WINDOW" IS DISPLAYED
+        // use only css when you inspect NEW WINDOW
+        // assertion should be assertEqual
+
+        WebElement link = driver.findElement(By.xpath("//a[@href='/windows/new']"));
+        link.click();
+
+       Set<String> windowIds = driver.getWindowHandles();
+
+       Iterator<String> iterator =  windowIds.iterator();
+
+       String parentWindowId = iterator.next();
+       String childWindowId=iterator.next();
+
+       driver.switchTo().window(childWindowId);
+
+       String  expectedResult ="New Window";
+       String actualResult = driver.findElement(By.cssSelector("div[class='example'] h3")).getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+
+
+
+
+
+    }
+
+
+
+
 
 
 }
